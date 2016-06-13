bd38daef9ff389540a4d02ad7eda0892aeff31ad
EugeneKey <evgeny.konyaev@gmail.com>
2016-06-14 01:46:16 +0300

    user can delete and edit own comment

--- spec/acceptance/comments/delete_comment_spec.rb
+++ spec/acceptance/comments/delete_comment_spec.rb
@@ -1,10 +1,16 @@
 require 'acceptance/acceptance_helper'
 
-feature 'Delete comment' do
-  given(:post) { create(:post) }
-  given!(:comment) { create(:comment, post: post) }
+feature 'Delete comment', '
+  Only the owner can remove the comment
 
-  scenario 'user delete comment', js: true do
+' do
+  given(:user) { create(:user) }
+  given(:another_user) { create(:user) }
+  given(:post) { create(:post) }
+  given!(:comment) { create(:comment, post: post, user: user) }
+
+  scenario 'Authenticated user delete own comment', js: true do
+    sign_in(user)
     visit post_path(post)
     expect(page).to have_content comment.body
     
@@ -14,5 +20,22 @@ feature 'Delete comment' do
 
     expect(page).to_not have_content comment.body
     expect(current_path).to eq post_path(post)
+  end
+
+  scenario 'Authenticated user cannot delete foreign comment' do
+    sign_in(another_user)
+    visit post_path(post)
+
+    within '.list-comments' do
+      expect(page).to_not have_link 'Delete'
+    end
+  end
+
+  scenario 'Non-authenticated
+   user cannot delete post' do
+    visit post_path(post)
+    within '.list-comments' do
+      expect(page).to_not have_link 'Delete'
+    end
   end
 end
--- spec/acceptance/comments/edit_comment_spec.rb
+++ spec/acceptance/comments/edit_comment_spec.rb
@@ -2,20 +2,27 @@ require 'acceptance/acceptance_helper'
 
 feature 'Comment editing', '
   In order to fix mistake
+  As an author comment
   I want to be able to edit my comment
 ' do
+  given(:user) { create(:user) }
+  given(:another_user) { create(:user) }
   given!(:post) { create(:post) }
-  given!(:comment) { create(:comment, post: post) }
+  given!(:comment) { create(:comment, post: post, user: user) }
+
+    describe 'Author' do
+    before do
+      sign_in user
+      visit post_path(post)
+    end
 
     scenario 'sees link to edit his comment' do
-      visit post_path(post)
       within '.comment-options' do
         expect(page).to have_link 'Edit'
       end
     end
 
     scenario 'try to edit his comment', js: true do
-      visit post_path(post)
       within '.comment-options' do
         click_on 'Edit'
       end
@@ -29,5 +36,23 @@ feature 'Comment editing', '
         expect(page).to_not have_selector 'textarea'
       end
     end
+  end
+
+  scenario "Authenticated user try to edit other user's comment" do
+    sign_in another_user
+    visit post_path(post)
+
+    within '.comment-options' do
+      expect(page).to_not have_link 'Edit'
+    end
+  end
+
+  scenario 'Non-authenticated user trying to edit answer' do
+    visit post_path(post)
+
+    within '.comment-options' do
+      expect(page).to_not have_link 'Edit'
+    end
+  end
 
 end

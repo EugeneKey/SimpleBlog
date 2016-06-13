d9c4aedf7a5823787303700623cbc731af372707
EugeneKey <evgeny.konyaev@gmail.com>
2016-06-14 01:19:35 +0300

    user can delete and edit own post

--- spec/acceptance/post/delete_post_spec.rb
+++ spec/acceptance/post/delete_post_spec.rb
@@ -1,10 +1,14 @@
 require 'acceptance/acceptance_helper'
 
-feature 'Delete post' do
-  given(:user) { create(:user) }
-  given(:post) { create(:post) }
+feature 'Delete post', '
+Only the owner can remove the post
 
-  scenario 'delete post' do
+' do
+  given(:user) { create(:user) }
+  given(:another_user) { create(:user) }
+  given(:post) { create(:post, user: user) }
+
+  scenario 'Authenticated user delete own post' do
     sign_in(user)
 
     visit post_path(post)
@@ -13,5 +17,18 @@ feature 'Delete post' do
     expect(page).to have_content 'Post was successfully destroyed.'
     expect(page).to_not have_content 'Title Post'
     expect(current_path).to eq posts_path
+  end
+
+  scenario 'Authenticated user cannot delete foreign post' do
+    sign_in(another_user)
+    visit post_path(post)
+
+    expect(page).to_not have_content 'Delete'
+  end
+
+  scenario 'Non-authenticated user cannot delete post' do
+    visit post_path(post)
+
+    expect(page).to_not have_content 'Delete'
   end
 end
--- spec/acceptance/post/edit_post_spec.rb
+++ spec/acceptance/post/edit_post_spec.rb
@@ -2,21 +2,25 @@ require 'acceptance/acceptance_helper'
 
 feature 'Post editing', '
   In order to fix mistake
+  As an author post
   I want to be able to edit post
 
 ' do
   given(:user) { create(:user) }
-  given!(:post) { create(:post) }
+  given(:another_user) { create(:user) }
+  given!(:post) { create(:post, user: user) }
 
-    scenario 'sees link to edit post' do
-      sign_in(user)
+  describe 'Author' do
+    before do
+      sign_in user
       visit post_path(post)
+    end
+
+    scenario 'sees link to edit his post' do
       expect(page).to have_link 'Edit'
     end
 
     scenario 'try to edit his post' do
-      sign_in(user)
-      visit post_path(post)
       click_on 'Edit'
       fill_in 'Title', with: 'New title his post'
       fill_in 'Body', with: 'New body text his post'
@@ -28,4 +32,18 @@ feature 'Post editing', '
       expect(page).to have_content 'New body text his post'
       expect(page).to_not have_selector 'edit_post'
     end
+  end
+  
+
+  scenario "Authenticated user try to edit other user's post" do
+    sign_in another_user
+    visit post_path(post)
+    expect(page).to_not have_link 'Edit'
+  end
+
+    scenario 'Non-authenticated user trying to edit post' do
+      visit post_path(post)
+
+      expect(page).to_not have_link 'Edit'
+  end
 end
